e83d9fcb29c23a3629151ca7e374f78f48e29216
Jayy277 <jaykanzariya153@gmail.com>
2026-06-14 22:37:34 +0530

    Added Pattern11,12 and 13

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,17 @@
+public class Pattern11 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(i+1);
+            }System.out.println("");
+        }
+    }
+}
+/*
+Output:
+11111
+22222
+33333
+44444
+55555
+*/
---
+++ Patterns/Pattern13.java
@@ -0,0 +1,21 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        int num=6;
+        for(int i=0;i<5;i++){
+            num--;
+            for(int j=0;j<5;j++){
+                System.out.print(num);
+                
+            }System.out.println(  "");
+        }
+}
+}
+/*
+Output:
+55555
+44444
+33333
+22222
+11111
+*/
+    
