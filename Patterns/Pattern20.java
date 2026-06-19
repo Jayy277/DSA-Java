df05ac8297ce1282c34fc8bac3524635b6cd2ae2
Jayy277 <jaykanzariya153@gmail.com>
2026-06-19 19:03:20 +0530

    Added Pattern20 and 21

---
+++ Patterns/Pattern20.java
@@ -0,0 +1,22 @@
+public class Pattern20 {
+    public static void main(String[] args) {
+        int n=4;
+        char ch='A';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n-i;j++){
+                System.out.print(ch);
+            }System.out.println("");
+            ch++;
+        }
+    }
+}
+/*
+Output:
+AAAA
+ BBB
+  CC
+   D
+*/
