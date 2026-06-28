4a646b9434721104f30b603604b0e225a3bce4c6
Jayy277 <jaykanzariya153@gmail.com>
2026-06-28 16:25:48 +0530

    _

--- Patterns/Pattern21.java
+++ Patterns/Pattern21.java
@@ -1,7 +1,6 @@
 public class Pattern21 {
     public static void main(String[] args) {
         int n=4;
-        int k=1;
         for(int i=0;i<n;i++){
              for(int j=0;j<n-i;j++){
                 System.out.print(" ");
