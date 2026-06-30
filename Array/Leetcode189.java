f9e1ff8168faa5795c0007986e6a171368fbecc0
Jayy277 <jaykanzariya153@gmail.com>
2026-06-30 23:15:14 +0530

    left rotate

---
+++ Array/Leetcode189.java
@@ -0,0 +1,20 @@
+class Leetcode189{
+    public static void main(String[] args) {
+    int nums[]={1,2,3,4,5,6,7};
+    int k=3;
+    int n=nums.length;
+        for(int i=0;i<k;i++){
+            int last=nums[n-1];
+            for(int j=n-1;j>0;j--){
+                nums[j]=nums[j-1];
+            }
+            nums[0]=last;
+        }
+
+        for(int l=0;l<nums.length;l++){
+            System.out.print(nums[l]+" ");
+        }
+    
+    }
+    
+}
