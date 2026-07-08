2caff491712685e2af9093d76b32b0aba6f39698
Jayy277 <jaykanzariya153@gmail.com>
2026-07-08 23:52:11 +0530

    Left Rotate Optimal Solution

--- Array/leftRotateByD.java
+++ Array/leftRotateByD.java
@@ -1,12 +1,25 @@
 public class leftRotateByD {
     
-    int nums[]={1,2,3,4,5,6,7,8,9};
-    int D=4;
+    static int nums[]={1,2,3,4,5,6,7,8,9};
+    static int D=4;
+    
     //Output: {5,6,7,8,9,1,2,3,4}
-    void reverse(int nums,int start,int end){
+    static void reverse(int[] nums,int start,int end){
+        while(start<=end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
 
+        }
     }
     public static void main(String[] args) {
-        
+        reverse(nums,0,D-1);
+        reverse(nums,D,nums.length-1);
+        reverse(nums,0,nums.length-1);
+        for (int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
     }
 }
